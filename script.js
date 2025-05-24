d100b44f2f21592cdd61e993b87e71b6451fcd59
dearoys <dea.roys@my.jcu.edu.au>
2025-05-24 10:57:01 +1000

    writing script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const name ="Dea Roys";
+let profession = "Programmer";
+
+if(name == "Dea Roys"){
+    //Execute this code
+    console.log("Name handle is Dea Roys");
+
+} else{
+    console.log(name,"is name handle");
+}
+
+const age = 18;
+if(age>=18){
+    console.log("I am allowed to vote");
+}
+if(age>=21){
+    console.log("I can drink alcohol");
+}
+else{
+    console.log("NOT old enough to drink alcohol");
+}
